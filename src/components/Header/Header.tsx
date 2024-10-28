20adc41153e69dc8fb83e492b4fe27cbff213f1e
humbuck3r <sr.ska89@gmail.com>
2024-10-28 20:35:57 -0300

    agrego el componente header al proyecto e instalo libreria swiper

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,35 @@
 import "./Header.css";
-export default function header() {
-  return <div>header</div>;
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+
+// Importa los módulos Navigation y Autoplay
+import { Navigation, Autoplay } from "swiper/modules";
+
+import Slider1 from "../../assets/Header slider1.png";
+
+export default function Header() {
+  return (
+    <Swiper
+      className="swiper-container"
+      modules={[Navigation, Autoplay]}
+      spaceBetween={30}
+      slidesPerView={1}
+      navigation
+      autoplay={{ delay: 6000, disableOnInteraction: false }}
+      loop={true}
+    >
+      <SwiperSlide>
+        <img src={Slider1} alt="Slide 1" className="swiper-img" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={Slider1} alt="Slide 2" className="swiper-img" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={Slider1} alt="Slide 3" className="swiper-img" />
+      </SwiperSlide>
+      {/* Agrega más SwiperSlides si es necesario */}
+    </Swiper>
+  );
 }
