d5302d5fb5f93d01aa992def4d0ec7019326cba7
humbuck3r <sr.ska89@gmail.com>
2024-10-28 19:59:24 -0300

    creo navbar y rama devEze

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "../src/components/NavBar/NavBar";
 import Header from "../src/components/Header/Header";
-import CardDelivery from "./components/CardDerlivery/CardDelivery";
-import Products from "../src/components/Products/Products";
-import Info from "../src/components/Info/Info";
-import Offer from "../src/components/Offer/Offer";
-import Footer from "./components/Footer/Footer";
+// import CardDelivery from "./components/CardDerlivery/CardDelivery";
+// import Products from "../src/components/Products/Products";
+// import Info from "../src/components/Info/Info";
+// import Offer from "../src/components/Offer/Offer";
+// import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <Router>
@@ -18,11 +18,11 @@ function App() {
           element={
             <>
               <Header />
-              <Products />
+              {/* <Products />
               <Info />
               <CardDelivery />
               <Offer />
-              <Footer />
+              <Footer /> */}
             </>
           }
         />
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,41 @@
 import "./NavBar.css";
-export default function navBar() {
-  return <div>navBar</div>;
+import Logo from "../../assets/logotienda.png";
+import ProfileIcon from "../../assets/iconprofile.png";
+import CartIcon from "../../assets/iconcart.png";
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <img src={Logo} alt="Logo" className="navbar-logo" />
+      <ul className="navbar-links-cont">
+        <a>
+          <li className="navbar-links">
+            <h2>Productos</h2>
+          </li>
+        </a>
+        <a>
+          <li className="navbar-links">
+            <h2>Sobre nosotros</h2>
+          </li>
+        </a>
+        <a>
+          <li className="navbar-links">
+            <h2>Como comprar?</h2>
+          </li>
+        </a>
+        <a>
+          <li className="navbar-links">
+            <h2>Trabajá con nosotros</h2>
+          </li>
+        </a>
+      </ul>
+      <div className="navbar-icon-container">
+        <a>
+          <img src={ProfileIcon} className="navbar-icon" />
+        </a>
+        <a>
+          <img src={CartIcon} className="navbar-icon" />
+        </a>
+      </div>
+    </div>
+  );
 }
