f00bdb6901b14dcb7654d4dbbe1da5aefc9b0219
Valentin Baldini <valentinbaldini28@gmail.com>
2025-09-08 12:09:49 -0300

    DIS-611 Tienda - Botones de comprar

--- front/src/constants/index.ts
+++ front/src/constants/index.ts
@@ -1,4 +1,4 @@
 // src/constants/index.ts
-// export const BASE_URL = "http://localhost:4000";
-export const BASE_URL = "https://www.administraciondistrolac.com";
+export const BASE_URL = "http://localhost:4000";
+// export const BASE_URL = "https://www.administraciondistrolac.com";
 export const AUTH_URL = "/api/store/auth";
